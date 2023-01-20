29c8b2c61148a92d950f2994953d11aba36102a8
Ornella B <borges.ornella@gmail.com>
2023-01-20 23:52:45 +0100

    suite exercice de comparaisons

---
+++ exercices.js
@@ -0,0 +1,34 @@
+/* const nbrUser= prompt('Entrez un chiffre entre 0 et 100') * 1
+console.log(nbrUser)
+const nbrPas = prompt('Entrez un pas') * 1
+
+for(let i = nbrUser; i > 0; i = i - nbrPas){
+    console.log(i)
+} */
+
+// demander de saisir 2 nbr par l'utilisateur 
+// si ils sont identique afficher dans la console 'les nbr sont egaux'
+// si il ne sont pas identique afficher lequel est le plus grand et lequel est le plus petit
+
+/* const nbrA = prompt('Entrez un nombre') * 1
+
+const nbrB = prompt('Entrez un nombre') * 1
+
+if(nbrA === nbrB){
+    console.log(`les nombres ${nbrA} et ${nbrB} sont egaux`)
+}
+
+else{
+    if(nbrA > nbrB){
+        console.log(`Le nombre ${nbrA} est plus grand que ${nbrB}`)
+    }
+    else{
+        console.log(`Le nombre ${nbrB} est plus grand que ${nbrA}`)
+    }
+} */
+ 
+
+// faire le même exercice mais avec trois nombres 
+// demander de saisir 3 nbr par l'utilisateur 
+// si ils sont identique afficher dans la console 'les nbr sont egaux'
+// si il ne sont pas identique afficher lequel est le plus grand et lequel est le plus petit
