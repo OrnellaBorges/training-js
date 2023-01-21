dcb4689ad0d5fac91f9d9018fc0dd3bd91299a23
Ornella B <borges.ornella@gmail.com>
2023-01-21 20:13:46 +0100

    exercices avec la méthode .sort

--- exercices.js
+++ exercices.js
@@ -31,4 +31,82 @@ else{
 // faire le même exercice mais avec trois nombres 
 // demander de saisir 3 nbr par l'utilisateur 
 // si ils sont identique afficher dans la console 'les nbr sont egaux'
-// si il ne sont pas identique afficher lequel est le plus grand et lequel est le plus petit
+
+/* const a = 2;
+const b = 2; 
+const c = 2;
+const tab = [7, 10, 1]
+
+if(a === b && b === c && c === a){
+    console.log('les trois variables sont identiques')
+}
+else if(a !== b && b !== c && c !== a){
+    console.log('les trois variables sont différentes')
+}
+else{
+    console.log('Deux des trois variables sont identiques')
+}
+tab.sort(function(a, b){
+    return b - a // ordre decroissant
+})
+
+tab.sort(function(a, b){// fonction anonyme
+    return a - b // ordre croissant
+})
+
+console.log(tab)
+
+// on peut aussi ecrire de la manière suivante : 
+function trierOrdreCroissant(a, b){
+    return a - b 
+}
+
+function trierOrdreDecroissant(a, b){
+    return b - a 
+}
+
+tab.sort(trierOrdreCroissant)
+console.log(tab) */
+// classer les nombres dans l'ordre croissant et dans l'ordre décroissant. ()
+
+const tab = [7, 10, 1, 15, 7, 72, 500, 98, 105]
+function trierOrdreCroissant(a, b){
+    return a - b 
+}
+tab.sort(trierOrdreCroissant)
+console.log(tab)
+
+
+/* //const months = ['Feb', 'Jan', 'Apr', 'Dec', 'Oct']
+const months = ['Jan', 'Feb', 'Apr', 'Dec', 'Oct']
+                                       a      b
+
+const months = ['Jan', 'Feb', 'Apr', 'Oct', 'Dec']
+console.log(months)
+
+function sortMonths(a, b){
+    const correctMonthsOrder = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    return correctMonthsOrder.indexOf(a) - correctMonthsOrder.indexOf(b);
+}                       11                            9
+
+months.sort(sortMonths)
+console.log(months) */
+
+
+
+const randomColors = ['Vert', 'Jaune', 'Noir', 'Blanc', 'Marine']
+
+const randomColorsCopy = [...randomColors] // faire une copie parfaite de la constante
+
+const randomColorsCopy2 = randomColors; 
+function sortColors(a, b){
+    const correctOrderColors = ['Blanc', 'Jaune', 'Vert', 'Marine', 'Noir']
+    return correctOrderColors.indexOf(a) - correctOrderColors.indexOf(b)
+}
+
+console.log('origin avant le sort: ', randomColors)
+randomColorsCopy2.sort(sortColors)
+console.log('copy2: ', randomColorsCopy2)
+console.log('original: ', randomColors)
+
+// 
